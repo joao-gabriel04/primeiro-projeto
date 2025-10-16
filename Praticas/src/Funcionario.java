fb855e9c589d2c5b420f69d974c893708dc28562
jotta <joanito822@gmail.com>
2025-10-16 10:41:32 -0300

    Teste

---
+++ Praticas/src/Funcionario.java
@@ -0,0 +1,14 @@
+public class Funcionario {
+   private String nome;
+   private String cpf;
+   private String endereco;
+   private double salario;
+
+   public Funcionario(String nome, String cpf, String endereco, double salario){
+       this.nome = nome;
+       this.cpf = cpf;
+       this.endereco = endereco;
+       this.salario = salario;
+   }
+
+}
---
+++ Praticas/src/TaxaDesconto.java
@@ -0,0 +1,5 @@
+public interface TaxaDesconto {
+
+   public void taxa(double valor);
+
+}
